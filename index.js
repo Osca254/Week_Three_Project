65a38e604f284e733c37a3efb2b5e45de8492d64
moringa <moringa@moringa>
2022-07-17 16:38:22 +0300

    modify index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var shades =["A","B","C","D","E"];
+var getMonths = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+for(i=0;i<=shades.length;i++){
+    console.log(shades)
+    
+}
+document.getElementsByClassName(getMonths)
